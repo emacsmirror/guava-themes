3b82e1814e2357146e636210518f5c68a53e06b7
Geralld Borbon <eternalmangocean@gmail.com>
2026-03-16 13:12:16 -0700

    fix(all): bump version to 0.11.6

--- themes/guava-themes-citrus-theme.el
+++ themes/guava-themes-citrus-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Geralld Borbón <eternalmangocean@gmail.com>
 ;; Created: Feb 15, 2026
-;; Version: 0.11.5
+;; Version: 0.11.6
 ;; Keywords: themes, faces, color
 ;; URL: http://github.com/bormoge/guava-themes
 ;; Package-Requires: ((emacs "24.1"))
