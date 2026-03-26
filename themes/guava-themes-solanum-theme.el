08979671f1a3a5c957404e13cbe0c1633b08bf03
Geralld Borbon <eternalmangocean@gmail.com>
2026-03-25 18:41:48 -0700

    chore: bump version to 0.12.1

--- themes/guava-themes-solanum-theme.el
+++ themes/guava-themes-solanum-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Geralld Borbón <eternalmangocean@gmail.com>
 ;; Created: Feb 22, 2026
-;; Version: 0.12.0
+;; Version: 0.12.1
 ;; Keywords: themes, faces, color
 ;; URL: http://github.com/bormoge/guava-themes
 ;; Package-Requires: ((emacs "24.1"))
